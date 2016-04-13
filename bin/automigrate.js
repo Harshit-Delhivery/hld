15105a2a5317cfc438532767f262609631110e1f
muditsatija1 <mudit.satija@delhivery.com>
2016-04-13 20:40:57 +0530

    migrations

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -1,6 +1,6 @@
 var server = require('../server/server.js');
 var ds = server.dataSources.localPsql;
-var lbTables = ['Attendance', 'Cancelled', 'Enduser', 'Offline', 'Orders'];
+var lbTables = ['Role', 'RoleMapping', 'ACL', 'AccessToken'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
