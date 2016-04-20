f55b860fad715b8be27411e29c9c3b32abad1d26
muditsatija1 <mudit.satija@delhivery.com>
2016-04-20 23:42:22 +0530

    dropdowns and many other features added

---
+++ client/js/controllers/home.controller.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var app = angular.module('hyperLocalDelivery');
+
+app.controller('homeController', ['$scope', '$state', '$http', 'notifyService', '$stateParams', '$rootScope', 'Restaurant', function($scope, $state, $http, notifyService, $stateParams, $rootScope, Restaurant) {
+	$scope.restaurant = null;
+	$scope.addRestaurant = function() {
+		Restaurant.create({
+			'merchantName': $scope.restaurant, 
+			'merchantId': 0, 
+			"dcName": $rootScope._user.dc_name,
+			"city": $rootScope._user.city },
+			function(successResponse) {
+				$scope.restaurant = null;
+				console.log('home controller successResponse = ', successResponse);
+				$scope.alertClass = 'alert alert-success alert-dismissible fade-in';
+				$scope.alertMessage = 'Attendance has been Successfully Submitted';
+			}, function(error) {
+				console.log(error);
+				$scope.alertClass = 'alert alert-danger alert-dismissible fade-in';
+				if(error.status == 422) {
+					$scope.alertMessage = 'Please fill in the Restaurant Name';
+				}
+			});
+	};
+}]);
--- client/js/controllers/offline.controller.js
+++ client/js/controllers/offline.controller.js
@@ -2,8 +2,9 @@
 
 var app = angular.module('hyperLocalDelivery');
 
-app.controller('offlineController', ['$scope', '$state', '$http', 'Enduser', 'notifyService', '$stateParams', '$rootScope', 'Offline', function($scope, $state, $http, Enduser,  notifyService, $stateParams, $rootScope, Offline) {
+app.controller('offlineController', ['$scope', '$state', '$http', 'Enduser', 'notifyService', '$stateParams', '$rootScope', 'Offline', 'Offreason', function($scope, $state, $http, Enduser,  notifyService, $stateParams, $rootScope, Offline, Offreason) {
 	$scope.date = null;
+	$scope.order_code = null;
 	$scope.merchant_id =null;
 	$scope.merchant_name =null;
 	$scope.client = null;
@@ -18,6 +19,7 @@ app.controller('offlineController', ['$scope', '$state', '$http', 'Enduser', 'no
 	$scope.submitOffline = function() {
 		Offline.create({
 		  "date": $scope.date,
+		  "order_code": $scope.order_code,
 		  "merchant_id": $scope.merchant_id,
 		  "merchant_name": $scope.merchant_name,
 		  "client": $scope.client,
@@ -34,6 +36,7 @@ app.controller('offlineController', ['$scope', '$state', '$http', 'Enduser', 'no
 			$scope.alertClass = 'alert alert-success alert-dismissible fade-in';
 			$scope.alertMessage = 'Offline Data has been Successfully Submitted';
 			$scope.date = null;
+			$scope.order_code = null;
 			$scope.merchant_id =null;
 			$scope.client = null;
 			$scope.fe_name = null;
@@ -83,6 +86,7 @@ app.controller('offlineController', ['$scope', '$state', '$http', 'Enduser', 'no
 		console.log('record = ', record);
 		Offline.updateAll({where: {id: record.id, dcName: $rootScope._user.dc_name, city: $rootScope._user.city}},
 			{	"merchant_id": record.merchant_id,
+				"order_code": record.order_code,
 				"merchant_name": record.merchant_name,
 				"client": record.client,
 				"fe_name": record.fe_name,
@@ -94,5 +98,14 @@ app.controller('offlineController', ['$scope', '$state', '$http', 'Enduser', 'no
 		}, function(error) {
 			console.log(error);
 		});
+	}
+
+	$scope.getOfflineReasons = function() {
+		Offreason.find({}, function(successResponse) {
+			console.log('getOfflineReasons = ', successResponse);
+			$rootScope._offlineReasons = successResponse;
+		}, function(error) {
+			console.log('getOfflineReasons = ', error);
+		})
 	}
 }]);
