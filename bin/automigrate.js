424d7ca73afca4833d2b315aef832a3637019b04
muditsatija1 <mudit.satija@delhivery.com>
2016-04-22 04:42:58 +0530

    pilot version-1

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -1,6 +1,6 @@
 var server = require('../server/server.js');
 var ds = server.dataSources.localPsql;
-var lbTables = [''];
+var lbTables = ['Source', 'Offline'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
