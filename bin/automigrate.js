d07feaa29b54668f98f8ec2b98fced5d1c86d8de
muditsatija1 <mudit.satija@delhivery.com>
2016-05-02 14:01:31 +0530

    auto migrate for prod dtata scource

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -1,5 +1,5 @@
 var server = require('../server/server.js');
-var ds = server.dataSources.localPsql;
+var ds = server.dataSources.prod;
 var lbTables = ['Hubmapping', 'Source', 'Offreason', 'Canreason', 'Restaurant', 'Role', 'RoleMapping', 'ACL', 'AccessToken', 'Attendance', 'Orders', 'Cancelled', 'Offline', 'Enduser'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
