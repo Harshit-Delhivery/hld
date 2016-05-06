5dcb34fbf37e3c98260128e0f868c38360cf59f1
muditsatija1 <mudit.satija@delhivery.com>
2016-05-06 15:51:58 +0530

    new restaurants added

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -1,7 +1,7 @@
 var server = require('../server/server.js');
 
 var ds = server.dataSources.prod;
-var lbTables = ['Hubmapping', 'Source', 'Offreason', 'Canreason', 'Restaurant', 'Role', 'RoleMapping', 'ACL', 'AccessToken', 'Attendance', 'Orders', 'Cancelled', 'Offline', 'Enduser'];
+var lbTables = ['Restaurant', 'Enduser'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
