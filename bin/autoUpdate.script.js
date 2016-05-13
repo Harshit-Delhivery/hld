f9dd49d62dfcc9deba9ee7fc5143741e6b5bf928
muditsatija1 <mudit.satija@delhivery.com>
2016-05-13 13:05:16 +0530

    auto update tables script added

--- bin/autoUpdate.script.js
+++ bin/autoUpdate.script.js
@@ -1,10 +1,10 @@
 var server = require('../server/server.js');
 
-var ds = server.dataSources.prod;
-var lbTables = ['Attendance', 'Offline'];
+var ds = server.dataSources.localPsql;
+var lbTables = ['Attendance'];
 ds.autoupdate(lbTables, function(er) {
   if (er) throw er;
-  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  console.log('Loopback tables [' + lbTables + '] updated in ', ds.adapter.name);
   ds.disconnect();
 });
 
