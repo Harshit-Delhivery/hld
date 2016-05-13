2b7511d13cb39b810cb78f5c33d31f5532240b0f
muditsatija1 <mudit.satija@delhivery.com>
2016-05-13 12:26:19 +0530

    auto update script addded

---
+++ bin/autoUpdate.script.js
@@ -0,0 +1,11 @@
+var server = require('../server/server.js');
+
+var ds = server.dataSources.prod;
+var lbTables = ['Attendance', 'Offline'];
+ds.autoupdate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
+
+['Hubmapping', 'Source', 'Offreason', 'Canreason', 'Restaurant', 'Role', 'RoleMapping', 'ACL', 'AccessToken', 'Attendance', 'Orders', 'Cancelled', 'Offline', 'Enduser']
